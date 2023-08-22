fd8ce8616fd84b20d4a216cec262abd0c32cfb35
Kirill Garmanov <k.garmanov@tinkoff.ru>
2023-08-22 17:57:49 +0300

    feat: add plugin for queue metrics exposing

---
+++ pkg/scheduler/plugins/queue-metrics/queue.go
@@ -0,0 +1,34 @@
+package queuemetrics
+
+import (
+	"volcano.sh/volcano/pkg/scheduler/framework"
+	"volcano.sh/volcano/pkg/scheduler/metrics"
+)
+
+// PluginName indicates name of volcano scheduler plugin.
+const PluginName = "queue-metrics"
+
+type queueMetricsPlugin struct {
+}
+
+// New return proportion action
+func New(_ framework.Arguments) framework.Plugin {
+	return &queueMetricsPlugin{}
+}
+
+func (pp *queueMetricsPlugin) Name() string {
+	return PluginName
+}
+
+func (pp *queueMetricsPlugin) OnSessionOpen(ssn *framework.Session) {
+	// Record metrics
+	for _, queue := range ssn.Queues {
+		metrics.UpdateQueuePodGroupInqueueCount(queue.Name, queue.Queue.Status.Inqueue)
+		metrics.UpdateQueuePodGroupPendingCount(queue.Name, queue.Queue.Status.Pending)
+		metrics.UpdateQueuePodGroupRunningCount(queue.Name, queue.Queue.Status.Running)
+		metrics.UpdateQueuePodGroupUnknownCount(queue.Name, queue.Queue.Status.Unknown)
+	}
+}
+
+func (pp *queueMetricsPlugin) OnSessionClose(ssn *framework.Session) {
+}
