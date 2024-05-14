df3f117148c40c32c29817039ed9682e391da3a3
Тимофей Разумов <t.e.razumov@tinkoff.ru>
2024-05-14 15:54:17 +0300

    fixes

--- pkg/scheduler/framework/statement.go
+++ pkg/scheduler/framework/statement.go
@@ -353,7 +353,6 @@ func (s *Statement) Discard() {
 	for i := len(s.operations) - 1; i >= 0; i-- {
 		op := s.operations[i]
 		op.task.GenerateLastTxContext()
-
 		switch op.name {
 		case Evict:
 			err := s.unevict(op.task)
@@ -379,7 +378,6 @@ func (s *Statement) Commit() {
 	klog.V(3).Info("Committing operations ...")
 	for _, op := range s.operations {
 		op.task.ClearLastTxContext()
-
 		switch op.name {
 		case Evict:
 			err := s.evict(op.task, op.reason)
