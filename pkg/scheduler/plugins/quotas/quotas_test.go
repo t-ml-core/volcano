680ab5b8d01a5e70b29b968ae3f506bbad37c3a5
Тимофей Разумов <t.e.razumov@tinkoff.ru>
2024-06-14 15:51:49 +0300

    fixes

--- pkg/scheduler/plugins/quotas/quotas_test.go
+++ pkg/scheduler/plugins/quotas/quotas_test.go
@@ -14,16 +14,8 @@ limitations under the License.
 package quotas
 
 import (
-	"io/ioutil"
-	"net/http"
-	"reflect"
-	"strconv"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/agiledragon/gomonkey/v2"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	apiv1 "k8s.io/api/core/v1"
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -38,8 +30,6 @@ import (
 	"volcano.sh/volcano/pkg/scheduler/cache"
 	"volcano.sh/volcano/pkg/scheduler/conf"
 	"volcano.sh/volcano/pkg/scheduler/framework"
-	"volcano.sh/volcano/pkg/scheduler/plugins/gang"
-	"volcano.sh/volcano/pkg/scheduler/plugins/priority"
 	"volcano.sh/volcano/pkg/scheduler/util"
 )
 
@@ -62,40 +52,6 @@ func getWorkerAffinity() *apiv1.Affinity {
 			},
 		},
 	}
-}
-
-func getLocalMetrics() int {
-	var data int
-
-	url := "http://127.0.0.1:8081/metrics"
-	method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return data
-	}
-	req.Header.Add("Authorization", "8cbdb37a-b880-4f2e-844c-e420858ea7eb")
-
-	res, err := client.Do(req)
-	if err != nil {
-		return data
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return data
-	}
-
-	split := strings.Split(string(body), "\n")
-	for _, v := range split {
-		if !strings.Contains(v, "#") && (strings.Contains(v, "volcano_queue_allocated_memory_bytes") || strings.Contains(v, "volcano_queue_allocated_milli_cpu")) {
-			data, _ = strconv.Atoi(strings.Split(v, " ")[1])
-		}
-	}
-
-	return data
 }
 
 type testParams struct {
@@ -132,166 +88,6 @@ func paramsToCache(t *testing.T, params testParams) *cache.SchedulerCache {
 	}
 
 	return schedulerCache
-}
-
-func TestQuotas(t *testing.T) {
-	c := make(chan bool, 1)
-	tmp := cache.NewMockSchedulerCache()
-
-	patches := gomonkey.ApplyMethod(reflect.TypeOf(tmp), "AddBindTask", func(scCache *cache.SchedulerCache, task *api.TaskInfo) error {
-		scCache.Binder.Bind(nil, []*api.TaskInfo{task})
-		return nil
-	})
-	defer patches.Reset()
-
-	patchUpdateQueueStatus := gomonkey.ApplyMethod(reflect.TypeOf(tmp), "UpdateQueueStatus", func(scCache *cache.SchedulerCache, queue *api.QueueInfo) error {
-		return nil
-	})
-	defer patchUpdateQueueStatus.Reset()
-
-	framework.RegisterPluginBuilder(PluginName, New)
-	framework.RegisterPluginBuilder(gang.PluginName, gang.New)
-	framework.RegisterPluginBuilder(priority.PluginName, priority.New)
-	options.ServerOpts = options.NewServerOption()
-	defer framework.CleanupPluginBuilders()
-
-	// Running pods
-	w1 := util.BuildPod("ns1", "worker-1", "", apiv1.PodRunning, api.BuildResourceList("3", "3k"), "pg1", map[string]string{"role": "worker"}, map[string]string{"selector": "worker"})
-	w2 := util.BuildPod("ns1", "worker-2", "", apiv1.PodRunning, api.BuildResourceList("5", "5k"), "pg1", map[string]string{"role": "worker"}, map[string]string{})
-	w3 := util.BuildPod("ns1", "worker-3", "", apiv1.PodRunning, api.BuildResourceList("4", "4k"), "pg2", map[string]string{"role": "worker"}, map[string]string{})
-	w1.Spec.Affinity = getWorkerAffinity()
-	w2.Spec.Affinity = getWorkerAffinity()
-	w3.Spec.Affinity = getWorkerAffinity()
-
-	// nodes
-	n1 := util.BuildNode("node1", api.BuildResourceList("4", "4k"), map[string]string{"selector": "worker"})
-	n2 := util.BuildNode("node2", api.BuildResourceList("3", "3k"), map[string]string{})
-	n1.Status.Allocatable["pods"] = resource.MustParse("15")
-	n2.Status.Allocatable["pods"] = resource.MustParse("15")
-	n1.Labels["kubernetes.io/hostname"] = "node1"
-	n2.Labels["kubernetes.io/hostname"] = "node2"
-
-	// priority
-	p1 := &schedulingv1.PriorityClass{ObjectMeta: metav1.ObjectMeta{Name: "p1"}, Value: 1}
-	p2 := &schedulingv1.PriorityClass{ObjectMeta: metav1.ObjectMeta{Name: "p2"}, Value: 2}
-	// podgroup
-	pg1 := &schedulingv1beta1.PodGroup{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "ns1",
-			Name:      "pg1",
-		},
-		Spec: schedulingv1beta1.PodGroupSpec{
-			Queue:             "q1",
-			MinMember:         int32(2),
-			PriorityClassName: p2.Name,
-		},
-	}
-	pg2 := &schedulingv1beta1.PodGroup{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "ns1",
-			Name:      "pg2",
-		},
-		Spec: schedulingv1beta1.PodGroupSpec{
-			Queue:             "q1",
-			MinMember:         int32(1),
-			PriorityClassName: p1.Name,
-		},
-	}
-	// queue
-	queue1 := &schedulingv1beta1.Queue{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "q1",
-		},
-	}
-
-	// tests
-	tests := []testParams{
-		{
-			name:  "pod-deallocate",
-			pods:  []*apiv1.Pod{w1, w2, w3},
-			nodes: []*apiv1.Node{n1, n2},
-			pcs:   []*schedulingv1.PriorityClass{p1, p2},
-			pgs:   []*schedulingv1beta1.PodGroup{pg1, pg2},
-			qs:    []*schedulingv1beta1.Queue{queue1},
-		},
-	}
-
-	for _, test := range tests {
-		// initialize schedulerCache
-		schedulerCache := paramsToCache(t, test)
-		// session
-		trueValue := true
-
-		num := 1
-		// proportion
-		go func() {
-			for {
-				ssn := framework.OpenSession(schedulerCache, []conf.Tier{
-					{
-						Plugins: []conf.PluginOption{
-							{
-								Name:             PluginName,
-								EnabledPredicate: &trueValue,
-							},
-							{
-								Name:                gang.PluginName,
-								EnabledJobReady:     &trueValue,
-								EnabledJobPipelined: &trueValue,
-							},
-							{
-								Name:            priority.PluginName,
-								EnabledJobOrder: &trueValue,
-							},
-						},
-					},
-				}, nil)
-				allocator := allocate.New()
-				allocator.Execute(ssn)
-				time.Sleep(time.Second * 3)
-				framework.CloseSession(ssn)
-				if num == 1 {
-					metrics := getLocalMetrics()
-					if metrics == 12000 {
-						t.Logf("init queue_allocated metrics is ok,%v", metrics)
-					}
-					schedulerCache.DeletePodGroupV1beta1(pg1)
-				} else if num == 2 {
-					metrics := getLocalMetrics()
-					if metrics == 4000 {
-						t.Logf("after delete vcjob pg1, queue_allocated metrics is ok,%v", metrics)
-					}
-					schedulerCache.DeletePodGroupV1beta1(pg2)
-				} else {
-					metrics := getLocalMetrics()
-					if metrics != 0 {
-						t.Errorf("after delete vcjob pg2, queue_allocated metrics is fail,%v", metrics)
-						c <- false
-						return
-					}
-					t.Logf("after delete vcjob pg2, queue_allocated metrics is ok,%v", metrics)
-					c <- true
-				}
-				num++
-			}
-		}()
-
-		go func() {
-			http.Handle("/metrics", promhttp.Handler())
-			err := http.ListenAndServe(":8081", nil)
-			if err != nil {
-				t.Errorf("ListenAndServe() err = %v", err.Error())
-			}
-		}()
-
-		for res := range c {
-			if !res {
-				t.Error("TestProportion failed")
-			} else {
-				t.Log("TestProportion successful")
-			}
-			return
-		}
-	}
 }
 
 func TestGuarantee(t *testing.T) {
