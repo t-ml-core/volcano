3349461f13f65e297f7d55a19db4a3040ec3f4a7
Тимофей Разумов <t.e.razumov@tinkoff.ru>
2024-08-02 17:21:48 +0300

    test

--- pkg/scheduler/plugins/quotas/quotas.go
+++ pkg/scheduler/plugins/quotas/quotas.go
@@ -396,6 +396,8 @@ func (p *quotasPlugin) OnSessionOpen(ssn *framework.Session) {
 		job := obj.(*api.JobInfo)
 		attr := p.queueOpts[job.Queue]
 
+		klog.V(4).Infof("AddJobEnqueueableFn: job_name: `%s`, len: `%d`, tasks: %v", job.Name, len(job.Tasks), job.Tasks)
+
 		if job.PodGroup.Spec.MinResources == nil {
 			klog.V(4).Infof("Job %s MinResources is null.", job.Name)
 			return util.Permit
